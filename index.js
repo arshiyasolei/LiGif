f91b553befbd1f5e376e41e263201d0c5bb04252
arshiyasolei <arshiyasoleimani@gmail.com>
2021-03-05 21:53:31 -0800

    fixed gifs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const discord = require("discord.js")
 let request = require('request');
 let app = new discord.Client()
@@ -19,35 +18,42 @@ function input(prompt, callback) {
 }
 
 app.on("ready", () => {
-	app.user.setActivity("taking a break from discord")
-	console.log(`Logged in as ${app.user.tag}!`)
+    app.user.setActivity("taking a break from discord")
+    console.log(`Logged in as ${app.user.tag}!`)
 })
 
 // Checks every message in every channel!
 app.on("message", message => {
-	
-	// for debugging purposes...
-	// console.log(message.content)
-    var mesg_tokens = message.content.split(" ");
-    for (i = 0; i < mesg_tokens.length; i++)
-    {
-        console.log(mesg_tokens[i]);
-        if (mesg_tokens[i].includes("https://lichess.org/" )) {
+
+    // for debugging purposes...
+    // console.log(message.content)
+    let mesg_tokens = message.content.split(" ");
+    for (i = 0; i < mesg_tokens.length; i++) {
+
+        if (mesg_tokens[i].includes("https://lichess.org/")) {
 
             // if the message is not from the bot!
-            if (!message.author.bot){
+            if (!message.author.bot) {
 
                 // get the redirected link!
                 let r = request.get(mesg_tokens[i], function (err, res, body) {
 
                     // if res exists
-                    if (res) {
+                    if (res.statusCode == 200) {
                         // use lichess gif api
-                        console.log(res.request.uri.href.split(' ')[0])
-                        let rr = res.request.uri.href.split(' ')[0].replace("https://lichess.org/","").replace("/black","").replace("/white","")
+                        //console.log(res.request.uri.href.split(' ')[0])
+                        let rr = res.request.uri.href.split(' ')[0].replace("https://lichess.org/", "").replace("/black", "").replace("/white", "")
 
-                        // send the message to the channel
-                        message.channel.send("https://lichess1.org/game/export/gif/" + rr + '.gif')
+                        // try seeing if your current pathway exists. 
+                        request.get("https://lichess1.org/game/export/gif/" + rr + '.gif', function (err, res, body) {
+
+                            if (res.statusCode == 200) {
+                                // send the message to the channel
+                                message.channel.send("https://lichess1.org/game/export/gif/" + rr + '.gif')
+
+                            }
+
+                        })
                     }
                 });
 
@@ -57,5 +63,6 @@ app.on("message", message => {
 })
 
 // get the API token from the user
-let token = input("Enter your discord bot token here: ", (user_input) => {app.login(user_input)});
-
+let token = input("Enter your discord bot token here: ", (user_input) => {
+    app.login(user_input)
+});
