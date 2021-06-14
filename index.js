8365a6f19518b58b4817889ebc531046ba98a3e5
arshiyasolei <arshiyasoleimani@gmail.com>
2021-06-13 20:10:35 -0700

    further clean ups

--- index.js
+++ index.js
@@ -15,18 +15,12 @@ app.on("message", async message => {
             todo_roundtwo = []
 
     // Go through each message and init a fetch call. 
-    for (let msg of mesg_tokens) {
-        if ((!message.author.bot) && msg.includes("https://lichess.org/")) {
-            try {
-                const s = fetch(msg);
-                todo_msgs.push(s);
-            } catch (error) {
-                console.log("another error")
-            }
-        }
+    for (const msg of mesg_tokens) {
+        if ((!message.author.bot) && msg.includes("https://lichess.org/")) 
+            todo_msgs.push(fetch(msg));
     }
     // await each fetch promise and make a new fetch request
-    for (let to of todo_msgs) {
+    for (const to of todo_msgs) {
         try {
             const   data = await to,
                     some_comp = data.url.split(' ')[0].replace("https://lichess.org/", "").replace("/black", "").replace("/white", ""),
@@ -38,14 +32,11 @@ app.on("message", async message => {
     }
     
     // Send to discord. This is the validation round. 
-    for (const [to, rr] of todo_roundtwo) {
-        try {
-            await to;
-            message.channel.send("https://lichess1.org/game/export/gif/" + rr + '.gif')
-        } catch (error) {
-            console.log("Error! ",error)
-        }
-    }
+    for (const [to, rr] of todo_roundtwo) 
+        await to.then(() => {
+            const path = "https://lichess1.org/game/export/gif/"
+            message.channel.send(path + rr + '.gif')},
+            error => console.log("Error! ",error))
 })
 
 
