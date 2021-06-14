12e08deea858d0761bdef97e9173b19a3ffb27c8
arshiyasolei <arshiyasoleimani@gmail.com>
2021-06-13 19:49:02 -0700

    Async/await~!

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 const discord = require("discord.js")
-let request = require('request');
-let app = new discord.Client()
-let readline = require('readline');
-
+const fetch = require("node-fetch"); 
+const app = new discord.Client()
 
 app.on("ready", () => {
-    app.user.setActivity("taking a break from discord")
+    app.user.setActivity("Thinking too hard...")
     console.log(`Logged in as ${app.user.tag}!`)
 })
 
 // Checks every message in every channel!
-app.on("message", message => {
+app.on("message", async message => {
 
-    // for debugging purposes...
-    // console.log(message.content)
-    let mesg_tokens = message.content.split(" ");
-    for (i = 0; i < mesg_tokens.length; i++) {
+    const   mesg_tokens = message.content.split(" "),
+            todo_msgs = [],
+            todo_roundtwo = []
 
-        if (mesg_tokens[i].includes("https://lichess.org/")) {
-
-            // if the message is not from the bot!
-            if (!message.author.bot) {
-
-                // get the redirected link!
-                let r = request.get(mesg_tokens[i], function (err, res, body) {
-
-                    // if res exists
-                    if (res.statusCode == 200) {
-                        // use lichess gif api
-                        //console.log(res.request.uri.href.split(' ')[0])
-                        let rr = res.request.uri.href.split(' ')[0].replace("https://lichess.org/", "").replace("/black", "").replace("/white", "")
-
-                        // try seeing if your current pathway exists. 
-                        request.get("https://lichess1.org/game/export/gif/" + rr + '.gif', function (err, res, body) {
-
-                            if (res.statusCode == 200) {
-                                // send the message to the channel
-                                message.channel.send("https://lichess1.org/game/export/gif/" + rr + '.gif')
-
-                            }
-
-                        })
-                    }
-                });
-
+    // Go through each message and init a fetch call. 
+    for (let msg of mesg_tokens) {
+        if ((!message.author.bot) && msg.includes("https://lichess.org/")) {
+            try {
+                const s = fetch(msg);
+                todo_msgs.push(s);
+            } catch (error) {
+                console.log("another error")
             }
         }
     }
+    // await each fetch promise and make a new fetch request
+    for (let to of todo_msgs) {
+        try {
+            const   data = await to,
+                    some_comp = data.url.split(' ')[0].replace("https://lichess.org/", "").replace("/black", "").replace("/white", ""),
+                    p = fetch("https://lichess1.org/game/export/gif/" + some_comp + '.gif');
+            todo_roundtwo.push([p,some_comp])
+        } catch (error) {
+            console.log("Error! ",error)
+        }
+    }
+    
+    // Send to discord. This is the validation round. 
+    for (const [to, rr] of todo_roundtwo) {
+        try {
+            await to;
+            message.channel.send("https://lichess1.org/game/export/gif/" + rr + '.gif')
+        } catch (error) {
+            console.log("Error! ",error)
+        }
+    }
 })
+
 
 // login with bottoken 
 app.login(process.env.BOTTOKEN)
